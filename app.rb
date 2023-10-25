07003239a00e1fc77eeaca6bdb1576365e8b5e5e
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-25 03:39:42 +0000

    final

--- app.rb
+++ app.rb
@@ -31,17 +31,27 @@ get("/wizard_subtract") do
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @second_num * @first_num
+  
   erb(:mult_result)
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
+
   erb(:div_result)
 end
